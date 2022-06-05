bb9d2bec72fff6e50c3cbe38bcede4ca0c4ffde3
Justus von der Beek <beek@in.tum.de>
2022-06-05 22:54:57 +0200

    Added storing and loading functions

--- database/database.go
+++ database/database.go
@@ -1,18 +1,47 @@
 package database
 
 import (
+	"Loxias/apollontypes"
 	"Loxias/packets"
-	"apollontypes"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"strings"
 )
 
 var database = make(map[uint32]apollontypes.User)
 
-func StoreInDatabase(user packets.Create, connection net.Conn) {
-	log.Println("Storing user in database")
+func PrintUser(user apollontypes.User) {
+	log.Printf("{ Username: %s, UserId: %d, Connection: nil }", user.Username, user.UserId)
+}
+
+func PrintDatabase() {
+	log.Println("--------------------------")
+	for _, v := range database {
+		PrintUser(v)
+	}
+	log.Println("--------------------------")
+}
+
+func CheckUser(user apollontypes.User) error {
+	if user.Username == "" {
+		log.Println("Cannot store user with empty username!")
+		return errors.New("Empty username")
+	}
+
+	if user.UserId == 0 {
+		log.Println("Cannot store user with user id 0!")
+		return errors.New("User ID 0")
+	}
+
+	return nil
+}
+
+func StoreInDatabase(user packets.Create, connection net.Conn) error {
+	// log.Println("Storing user in database")
 
 	newUser := apollontypes.User{
 		Username:   user.Username,
@@ -20,16 +49,38 @@ func StoreInDatabase(user packets.Create, connection net.Conn) {
 		Connection: connection,
 	}
 
+	err := CheckUser(newUser)
+	if err != nil {
+		return err
+	}
+
 	_, exists := database[user.UserId]
 
 	if exists {
 		log.Printf("User with ID %d already exists", user.UserId)
-		return
+		return errors.New("User already exists")
 	}
 
 	database[user.UserId] = newUser
 
 	log.Printf("Stored user \"%s\" with id \"%d\"", user.Username, user.UserId)
+	return nil
+}
+
+func StoreUserInDatabase(user apollontypes.User) error {
+	// log.Println("Storing user in database")
+	err := CheckUser(user)
+	if err != nil {
+		return err
+	}
+	_, exists := database[user.UserId]
+	if exists {
+		log.Printf("User with ID %d already exists", user.UserId)
+		return errors.New("User already exists")
+	}
+	database[user.UserId] = user
+	log.Printf("Stored user \"%s\" with id \"%d\"", user.Username, user.UserId)
+	return nil
 }
 
 func SearchUsers(search string) []packets.Contact {
@@ -69,8 +120,69 @@ func IdExists(id uint32) bool {
 	return exists
 }
 
-func PrintDatabase() {
-	log.Println("Database:\n--------------------------")
-	log.Printf("%s", database)
-	log.Println("---------------------")
+func Clear() {
+	database = make(map[uint32]apollontypes.User)
+}
+
+func ConvertToByte() ([]byte, error) {
+	var content []byte
+	var err error
+	err = nil
+	seperator := ","
+	counter := 0
+	for _, v := range database {
+		raw, err := json.Marshal(v)
+		if err != nil {
+			log.Printf("Failed to convert user %d to byte", v.UserId)
+			err = errors.New("Not all users converted")
+			continue
+		}
+		// The '...' signal that all elements of raw should be appended
+		content = append(content, raw...)
+		if counter+1 < len(database) {
+			content = append(content, []byte(seperator)...)
+		}
+		counter++
+	}
+	return content, err
+}
+
+func SaveToFile(file string) error {
+	log.Printf("Saving to \"%s\"", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Printf("Failed to create file \"%s\"", file)
+		return err
+	}
+	// Don't forget to close the file
+	defer f.Close()
+	users, err := ConvertToByte()
+	if err != nil {
+		log.Println("Failed to save users to file!")
+		return err
+	}
+	f.Write([]byte("["))
+	f.Write(users)
+	f.Write([]byte("]"))
+	return nil
+}
+
+func ReadFromFile(file string) error {
+	Clear()
+	log.Printf("Reading from \"%s\"", file)
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	var data []apollontypes.User
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	for _, v := range data {
+		database[v.UserId] = v
+	}
+	return nil
 }
