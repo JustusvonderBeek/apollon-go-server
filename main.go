bdf196835b033cddc2a577494c6815e6085bd458
Justus von der Beek <beek@in.tum.de>
2022-09-05 15:50:01 +0200

    Added flags and logfile

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"Loxias/server"
 	"flag"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
 	// Parsing the cmdline
 	securePtr := flag.Bool("t", false, "Enable TLS")
+	addr := flag.String("a", "0.0.0.0", "Listen address")
+	port := flag.String("p", "50000", "Listen port")
+	logfile := flag.String("l", "server.log", "The logfile")
 	flag.Parse()
 
-	server.Start(*securePtr)
+	setupLogger(*logfile)
+	server.Start(*addr, *port, *securePtr)
+}
+
+func setupLogger(logfile string) {
+	logFile, err := os.OpenFile(logfile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
 }
