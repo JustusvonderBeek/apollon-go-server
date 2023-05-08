b069f4c4e6613f62aaeeeda29405a806d51e90dd
Justus von der Beek <beek@in.tum.de>
2023-05-08 19:59:30 +0900

    Started implementing new header

--- apollon/apollon.go
+++ apollon/apollon.go
@@ -2,51 +2,21 @@ package apollon
 
 import (
 	"Loxias/database"
-	"Loxias/packets"
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"encoding/hex"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"log"
 	"math"
 	"math/rand"
 	"net"
 	"os"
 	"time"
+
+	"apollon.chat.com/packets"
 )
-
-func CreatePacket(content any) ([]byte, error) {
-	packet, err := json.Marshal(content)
-	if err != nil {
-		return nil, err
-	}
-	if len(packet) > 65534 {
-		log.Println("Packet longer than 16 kB are not supported")
-		return nil, errors.New("Packet too long")
-	}
-	size := uint16(len(packet) + 2)
-	buffer := make([]byte, size)
-	binary.BigEndian.PutUint16(buffer, size)
-	copied := copy(buffer[2:], packet)
-	if copied < len(packet) {
-		log.Printf("Something went wrong during packet creation! Should copy %d, but copied only %d", size, copied)
-		return nil, errors.New("Failed to copy all data")
-	}
-	// log.Printf("Packet: %02x", buffer)
-	return buffer, nil
-}
-
-// func HandleIncoming(connection net.Conn, read chan []byte) {
-// 	log.Println("Starting incoming listener...")
-// 	for {
-// 		// Must be already completly read packets (including size info)
-// 		packet := <-read
-// 		connection.Write(packet)
-// 	}
-// }
 
 func HandleOldMessages(id uint32, connection net.Conn) {
 	log.Printf("Handling messages for \"%d\"", id)
@@ -55,7 +25,14 @@ func HandleOldMessages(id uint32, connection net.Conn) {
 		log.Printf("No messages for client \"%d\" found", id)
 	} else {
 		for _, v := range messages {
-			raw, err := CreatePacket(v)
+			// TODO: Fix this, for now this is only to fix the compiliation error
+			header := packets.Header{
+				Category:  packets.CAT_DATA,
+				Type:      packets.D_TEXT,
+				UserId:    id,
+				MessageId: messages[0].ContactUserId,
+			}
+			raw, err := packets.SerializePacket(header, v)
 			if err != nil {
 				continue
 			}
@@ -77,10 +54,11 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 	reader := bufio.NewReader(connection)
 	var id uint32
 	id = 0
-	sizeBuf := make([]byte, 2)
+	headerBuffer := make([]byte, 10)
 
 	for {
-		read, err := reader.Read(sizeBuf)
+		// read, err := reader.ReadString('\n') // TODO: Switch to this version.
+		read, err := reader.Read(headerBuffer)
 
 		// log.Printf("%T %+v", err, err) // checking err type
 		if read == 0 {
@@ -94,56 +72,29 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 			continue
 		}
 
-		// log.Printf("Read %d bytes size information from the client", read)
-
-		size := binary.BigEndian.Uint16(sizeBuf)
-		log.Printf("Expecting %d bytes of data", size)
-		if size <= 4 {
-			// TODO: Define specific keep alive packet
-			log.Println("Keep alive packet")
-			continue
+		if read != 10 {
+			log.Fatal("The first packet was not enough to fit the header!")
 		}
 
-		// ADAPT SIZE INFORMATION IF SIZE FIELD CHANGES
-		contentBuf := make([]byte, size-2)
-		read, err = reader.Read(contentBuf)
+		// log.Printf("Raw: %x", headerBuffer)
+		// log.Printf("Raw String: %s", string(headerBuffer))
 
-		if read == 0 {
-			log.Printf("Connection \"%d\" closed by remote host", id)
-			delete(db, id)
-			return
-		}
-
-		if err != nil {
-			log.Println("Failed to read data from client!")
-			continue
-		}
-
-		// log.Printf("Raw: %x", contentBuf)
-		// log.Printf("Raw String: %s", string(contentBuf))
-
-		// category, typ, err := packets.PacketType(contentBuf)
-		// packet, err := packets.DeseralizePacket(contentBuf)
+		// Decode the header information
 		var header packets.Header
-		err = json.Unmarshal(contentBuf, &header)
+		newReader := bytes.NewReader(headerBuffer)
+		err = binary.Read(newReader, binary.BigEndian, &header)
 		if err != nil {
 			log.Println("Failed to extract header information from packet")
-			if len(contentBuf) > 100 {
-				log.Printf("Content (first 100 bytes): %x", contentBuf[:100])
-			} else {
-				log.Printf("Content: %x", contentBuf[:])
-			}
 			// TODO: Is this rather due to an transmission error or because the client send wrong information? This should NORMALLY only happen if the client is malicous and sends incorrect data as the first part of the packet -> return
 			log.Printf("Set client %d to nil", id)
 			delete(db, id)
 			return
 		}
 		id = header.UserId
-		log.Printf("Packet:\n%s", hex.Dump(contentBuf))
+		log.Printf("Header:\n%s", hex.Dump(headerBuffer))
 
 		if id > 0 {
 			db[id] = connection
-			// TODO: In order to trigger this right at the beginning we would need something as a login message with an id in it
 			go HandleOldMessages(id, connection)
 		}
 
@@ -153,14 +104,22 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 		case packets.CAT_CONTACT:
 			switch header.Type {
 			case packets.CON_CREATE:
+				// Reading the actual payload
+				payload, err := reader.ReadSlice('\n')
+				if err != nil {
+					log.Printf("Failed to read payload of create packet!\n%s", err)
+					continue
+				}
 				var create packets.Create
-				create, err = packets.DeseralizePacket[packets.Create](contentBuf)
+				create, err = packets.DeseralizePacket[packets.Create](payload)
+
 				if err != nil {
 					// This only happens if incorrect JSON was send
 					delete(db, id)
 					return
 				}
 				// Generate new user id
+				// TODO: Make faster in case most IDs are used
 				newUserId := rand.Uint32()
 				safeCounter := math.MaxInt32
 				for {
@@ -171,38 +130,40 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 					safeCounter--
 					newUserId = rand.Uint32()
 				}
-				create.UserId = newUserId
+				header.UserId = newUserId
 				// Store new user in some sort of database
-				err = database.StoreInDatabase(create)
+				err = database.StoreInDatabase(newUserId, create.Username)
 				if err != nil {
 					// Failed to insert user into database
 					continue
 				}
 				// Sending back the ID to the client
-				encoded, err := CreatePacket(create)
+				encoded, err := packets.SerializePacket(header, nil)
 				if err != nil {
 					log.Println("Failed to encode answer")
 					continue
 				}
 				connection.Write(encoded)
 			case packets.CON_SEARCH:
-				var search packets.Search
-				search, err = packets.DeseralizePacket[packets.Search](contentBuf)
+				payload, err := reader.ReadSlice('\n')
+
 				if err != nil {
-					log.Println("Failed to deserialize packet")
+					log.Printf("Failed to read payload of search packet!%s\n", err)
+					continue
+				}
+
+				var search packets.Search
+				search, err = packets.DeseralizePacket[packets.Search](payload)
+				if err != nil {
+					log.Println("Failed to deserialize search payload")
 					delete(db, id)
 					return
 				}
 				users := database.SearchUsers(search.UserIdentifier)
 				log.Printf("%d users for identifier \"%s\" found", len(users), search.UserIdentifier)
-				contactList, err := packets.CreateContactList(search, users)
-				if err != nil {
-					// Internal error, tried our best
-					log.Println("Contact list could not be created")
-					continue
-				}
+				header, contactList := packets.CreateContactList(header.UserId, header.MessageId, users)
 
-				encoded, err := CreatePacket(contactList)
+				encoded, err := packets.SerializePacket(header, contactList)
 				if err != nil {
 					log.Println("Failed to encode contact list")
 					continue
@@ -215,7 +176,12 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 				delete(db, id)
 				return
 			case packets.CON_OPTION:
-				option, err := packets.DeseralizePacket[packets.ContactOption](contentBuf)
+				payload, err := reader.ReadSlice('\n')
+				if err != nil {
+					log.Printf("Failed to read payload of contact option packet!%s\n", err)
+					continue
+				}
+				option, err := packets.DeseralizePacket[packets.ContactOption](payload)
 				if err != nil {
 					log.Println("Failed to deserialize packet!")
 					delete(db, id)
@@ -227,49 +193,40 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 					// TODO: Save question to file
 					forwardCon = nil
 				}
-				err = HandleContactOption(option, connection, forwardCon)
+				err = HandleContactOption(header, option, connection, forwardCon)
 				if err != nil {
 					delete(db, id)
 					return
 				}
 			case packets.CON_LOGIN:
-				login, err := packets.DeseralizePacket[packets.Login](contentBuf)
-				if err != nil {
-					log.Println("Failed to deserialize login packet!")
-					delete(db, id)
-					return
-				}
 				// Nothing else to do here. The client is already inserted into the database and the login packet seems to have the correct format!
-				log.Printf("Login from user %d", login.UserId)
+				log.Printf("Login from user %d", header.UserId)
 			case packets.CON_CONTACT_INFO:
-				contact, err := packets.DeseralizePacket[packets.ContactInfo](contentBuf)
+				payload, err := reader.ReadSlice('\n')
+				if err != nil {
+					log.Printf("Failed to read payload of contact info packet!%s\n", err)
+					continue
+				}
+				contact, err := packets.DeseralizePacket[packets.ContactInfo](payload)
 				if err != nil {
 					log.Println("Failed to deserialize contact information packet!")
 					delete(db, id)
 					return
 				}
 				// log.Printf("Got contact information: %s", string(contentBuf))
-				log.Printf("Got data from %d, expecting image of size %d", contact.UserId, contact.ImageBytes)
-				imageBuffer := make([]byte, contact.ImageBytes)
-				// Variant to wait for the full image
-				// TODO: Create a timeout for this method, otherwise one could make this wait infinite
-				read, err = io.ReadFull(reader, imageBuffer)
-				if err != nil {
-					log.Printf("Failed to read image from remote!\n%s", err)
-				}
-				log.Printf("Got %d bytes, first 100 image bytes %x", read, hex.Dump(imageBuffer[:100]))
+
+				// Forwarding to first friend currently TODO:
 				forwardCon, ex := db[contact.ContactIds[0]]
 				if !ex {
 					log.Printf("Contact %du not online", contact.ContactIds[0])
 					continue
 				}
-				forward, err := CreatePacket(contact)
+				forward, err := packets.SerializePacket(header, contact)
 				if err != nil {
 					log.Print("Failed to serialize contact packet")
 					continue
 				}
 				forwardCon.Write(forward)
-				forwardCon.Write(imageBuffer)
 				log.Print("Forwarded image to %du", contact.ContactIds[0])
 			default:
 				log.Printf("Incorrect packet type: %d", header.Type)
@@ -279,8 +236,13 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 		case packets.CAT_DATA:
 			switch header.Type {
 			case packets.D_TEXT:
+				payload, err := reader.ReadSlice('\n')
+				if err != nil {
+					log.Printf("Failed to read payload of contact info packet!%s\n", err)
+					continue
+				}
 				var text packets.Text
-				text, err = packets.DeseralizePacket[packets.Text](contentBuf)
+				text, err = packets.DeseralizePacket[packets.Text](payload)
 				log.Printf("Got \"%s\" forwarding to \"%d\"", text.Message, text.ContactUserId)
 				if err != nil {
 					log.Println("Failed to deserialize text packet")
@@ -290,21 +252,16 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 
 				// First write the ack back to the sending client (later on save the text and send to client when it comes back online)
 				textAck := packets.TextAck{
-					Category:      packets.CAT_DATA,
-					Type:          packets.D_TEXT_ACK,
-					UserId:        text.ContactUserId,
-					MessageId:     text.MessageId,
-					ContactUserId: text.UserId,
+					ContactUserId: header.UserId,
 					Timestamp:     text.Timestamp,
-					AckPart:       text.Part,
 				}
-				ack, err := CreatePacket(textAck)
+				ack, err := packets.SerializePacket(header, textAck)
 				if err != nil {
 					log.Println("Failed to create ack packet")
 					continue
 				}
 				connection.Write(ack)
-				log.Printf("Wrote textAck back to %d", text.UserId)
+				log.Printf("Wrote textAck back to %d", header.UserId)
 
 				// Continue with forwarding the text
 				forwardCon, ex := db[text.ContactUserId]
@@ -314,7 +271,7 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 					database.SaveMessagesToFile(text, fmt.Sprint(text.ContactUserId)+".json")
 					continue
 				}
-				forward, err := CreatePacket(text)
+				forward, err := packets.SerializePacket(header, text)
 				if err != nil {
 					log.Printf("Failed to create forward packet!")
 					continue
@@ -337,14 +294,14 @@ func HandleClient(connection net.Conn, db map[uint32]net.Conn) {
 	}
 }
 
-func HandleContactOption(option packets.ContactOption, connection net.Conn, forwardCon net.Conn) error {
+func HandleContactOption(header packets.Header, option packets.ContactOption, connection net.Conn, forwardCon net.Conn) error {
 	for _, v := range option.Options {
 		log.Printf("Option: {%s, %s}", v.Type, v.Value)
 		switch v.Type {
 		case "Question":
 			switch v.Value {
 			case "Add":
-				log.Printf("User %d wants to add %d", option.UserId, option.ContactUserId)
+				log.Printf("User %d wants to add %d", header.UserId, option.ContactUserId)
 				_, err := database.GetUser(option.ContactUserId)
 				if err != nil {
 					log.Printf("%s", err)
@@ -353,7 +310,7 @@ func HandleContactOption(option packets.ContactOption, connection net.Conn, forw
 
 				// TODO: Add forwarding the request to be able to automatically add the user into the list
 				if forwardCon != nil {
-					forwardPacket, err := CreatePacket(option)
+					forwardPacket, err := packets.SerializePacket(header, option)
 					if err != nil {
 						log.Print("Failed to create Option packet to forward!")
 						break
@@ -382,20 +339,22 @@ func HandleContactOption(option packets.ContactOption, connection net.Conn, forw
 				}
 				nameOption := packets.Option{
 					Type:  "Name",
-					Value: "username",
+					Value: option.Options[len(option.Options)-1].Value,
 				}
 				options := make([]packets.Option, 2)
 				options[0] = answerOption
 				options[1] = nameOption
+				answerHeader := packets.Header{
+					Category:  packets.CAT_CONTACT,
+					Type:      packets.CON_OPTION,
+					UserId:    option.ContactUserId,
+					MessageId: header.MessageId,
+				}
 				accept := packets.ContactOption{
-					Category:      packets.CAT_CONTACT,
-					Type:          packets.CON_OPTION,
-					UserId:        option.ContactUserId,
-					MessageId:     option.MessageId,
-					ContactUserId: option.UserId,
+					ContactUserId: header.UserId,
 					Options:       options,
 				}
-				packet, err := CreatePacket(accept)
+				packet, err := packets.SerializePacket(answerHeader, accept)
 				if err != nil {
 					log.Println("Failed to create answer packet!")
 					break
@@ -410,15 +369,17 @@ func HandleContactOption(option packets.ContactOption, connection net.Conn, forw
 				}
 				options := make([]packets.Option, 1)
 				options[0] = removeAck
+				answerHeader := packets.Header{
+					Category:  packets.CAT_CONTACT,
+					Type:      packets.CON_OPTION,
+					UserId:    option.ContactUserId,
+					MessageId: header.MessageId,
+				}
 				ack := packets.ContactOption{
-					Category:      packets.CAT_CONTACT,
-					Type:          packets.CON_OPTION,
-					UserId:        option.ContactUserId,
-					MessageId:     option.MessageId,
-					ContactUserId: option.UserId,
+					ContactUserId: header.UserId,
 					Options:       options,
 				}
-				packet, err := CreatePacket(ack)
+				packet, err := packets.SerializePacket(answerHeader, ack)
 				if err != nil {
 					log.Printf("Failed to create next packet")
 					break
