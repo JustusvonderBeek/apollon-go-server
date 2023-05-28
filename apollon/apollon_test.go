c6210fdfecf166f20c82c88e3ef79f90de71109c
Justus von der Beek <beek@in.tum.de>
2023-05-28 23:11:49 +0900

    Check for contact info

--- apollon/apollon_test.go
+++ apollon/apollon_test.go
@@ -209,8 +209,8 @@ func TestSendingMessage(t *testing.T) {
 	messageId := rand.Uint32()
 	text := "Testing sending message"
 	// First only sending text (MUST FAIL!)
-	createHeader, create := packets.CreateText(userId, messageId, contactId, text)
-	textPacket, err := packets.SerializePacket(createHeader, create)
+	createTextHeader, createText := packets.CreateText(userId, messageId, contactId, text)
+	textPacket, err := packets.SerializePacket(createTextHeader, createText)
 	if err != nil {
 		log.Printf("Internal Failure while serializing the packet!")
 		t.FailNow()
@@ -292,6 +292,117 @@ func TestSendingMessage(t *testing.T) {
 		t.FailNow()
 	}
 	if ackHeader.Type != packets.D_TEXT_ACK {
+		t.FailNow()
+	}
+	if ackHeader.UserId != userId {
+		t.FailNow()
+	}
+	if ackHeader.MessageId != messageId {
+		t.FailNow()
+	}
+}
+
+func TestUpdateContactInfo(t *testing.T) {
+	userId := uint32(1293812414)
+	messageId := rand.Uint32()
+	username := "Username"
+	image := []byte{0x01, 0x02, 0x03, 0x04}
+	userlist := []uint32{3718291512}
+
+	// First only sending contact info (MUST FAIL!)
+	contactInfoHeader, contactInfo := packets.CreateContactInfo(userId, messageId, username, image, userlist)
+	packet, err := packets.SerializePacket(contactInfoHeader, contactInfo)
+	if err != nil {
+		log.Printf("Internal Failure while serializing the packet!")
+		t.FailNow()
+	}
+	addr := "127.0.0.1" + ":" + "50000"
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Printf("Failed to connect to the server!")
+		t.FailNow()
+	}
+	defer conn.Close()
+	conn.Write(packet)
+
+	time.Sleep(100 * time.Millisecond)
+
+	// Expecting to fail!
+	headerBuffer := make([]byte, 10)
+	// Timeout after 2 seconds
+	conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
+	read, err := conn.Read(headerBuffer)
+	if err == nil || read == 10 {
+		log.Printf("Received answer back!")
+		t.FailNow()
+	}
+	var netErr net.Error
+	if err != nil && errors.As(err, &netErr) && netErr.Timeout() {
+		log.Printf("Timeout instead of connection closed!")
+		t.FailNow()
+	}
+
+	// Now, with login
+	userId = uint32(1293812414)
+	// Create the login package and send it to the other end
+	loginHeader := packets.CreateLogin(userId, rand.Uint32())
+	login, err := packets.SerializePacket(loginHeader, nil)
+	if err != nil {
+		log.Printf("Internal Failure while serializing the packet!")
+		t.FailNow()
+	}
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		log.Printf("Failed to connect to the server!")
+		t.FailNow()
+	}
+	defer conn.Close()
+	wrote, err := conn.Write(login)
+	if err != nil {
+		log.Printf("Failed to write to server!\n%s", err)
+		t.FailNow()
+	}
+	if wrote != len(login) {
+		log.Printf("Failed to write all %d bytes to server! Wrote only %d", len(login), wrote)
+		t.FailNow()
+	}
+	// Waiting for server to close connection
+	time.Sleep(100 * time.Millisecond)
+
+	wrote, err = conn.Write(packet)
+	if err != nil {
+		log.Printf("Failed to write to server!\n%s", err)
+		t.FailNow()
+	}
+	if wrote != len(packet) {
+		log.Printf("Failed to sent whole packet to server. Sent only %d", wrote)
+		t.FailNow()
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
+	read, err = conn.Read(headerBuffer)
+	if err != nil || read != 10 {
+		log.Printf("Did not receive answer packet back!")
+		t.FailNow()
+	}
+	if err != nil && errors.As(err, &netErr) && netErr.Timeout() {
+		log.Printf("Timeout instead of receiving data!")
+		t.FailNow()
+	}
+	var ackHeader packets.Header
+	newReader := bytes.NewReader(headerBuffer)
+	err = binary.Read(newReader, binary.BigEndian, &ackHeader)
+	if err != nil {
+		log.Printf("Failed to decode header: %s", err)
+		t.FailNow()
+	}
+
+	if ackHeader.Category != packets.CAT_CONTACT {
+		t.FailNow()
+	}
+	if ackHeader.Type != packets.CON_CONTACT_ACK {
 		t.FailNow()
 	}
 	if ackHeader.UserId != userId {
